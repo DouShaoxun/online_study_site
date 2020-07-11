2282d69de06c327ebc0d43c95b3f7280655d2ffd
DouShaoxun <dsx1046811165@gmail.com>
2020-07-12 00:25:35 +0800

    jwt

--- pages/login.vue
+++ pages/login.vue
@@ -45,6 +45,8 @@
 <script>
 import '~/assets/css/sign.css'
 import '~/assets/css/iconfont.css'
+import cookie from 'js-cookie'
+import loginApi from '~/api/login'
 
 export default {
   layout: 'sign',
@@ -61,7 +63,13 @@ export default {
   methods: {
     // 登录
     submitLogin() {
-
+      // 执行远程登录接口调用
+      loginApi.submitLogin(this.user).then(response => {
+        // 将jwt写入cookie
+        cookie.set('jwt_token', response.data.token, { domain: 'localhost' })
+        // 跳转到网站的首页面
+        window.location.href = '/'
+      })
     }
   }
 }
